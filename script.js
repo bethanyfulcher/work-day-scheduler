c223fd54cf655c14dea7b4261bd11bb392e66a2b
Bethany Fulcher <fulcherbethany@gmail.com>
2020-12-16 16:31:02 -0800

    set up layout in html

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO: find and store a time object into a variable
+// TODO: display date at the top of the page
+
+// TODO: set all values less than the time to be in the past
+    // TODO: add attribute class= "past" to element
+// TODO: set all values greater than the time to be in the future
+    // TODO: add attribute class= "future" to element
+// TODO: set value that equals the time to be present
+    // TODO: add attribute class= "present" to element
+
+// TODO: add click event on save buttons
+    // TODO: add to local storage assigned to certain date
+    // TODO: render all assigned events when page refreshes
