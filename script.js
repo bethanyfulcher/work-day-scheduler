e0a8ba0eb641bab7636da5c26442eb1d77045597
Bethany Fulcher <fulcherbethany@gmail.com>
2020-12-16 17:18:51 -0800

    added date to display current date

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
+
+
 // TODO: find and store a time object into a variable
+var time = luxon.DateTime.local()
+console.log(time)
+var dayOfTheWeek = time.weekdayLong
+var day = time.day
+var month = time.monthLong
+var date = dayOfTheWeek + ", " + month + " " + day
+console.log(date)
 // TODO: display date at the top of the page
+
+$("#currentDay").text(date)
 
 // TODO: set all values less than the time to be in the past
     // TODO: add attribute class= "past" to element
@@ -11,3 +23,4 @@
 // TODO: add click event on save buttons
     // TODO: add to local storage assigned to certain date
     // TODO: render all assigned events when page refreshes
+
